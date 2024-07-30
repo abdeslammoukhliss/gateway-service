6a25e10876710ac2bbe7abc492c406971a2a0e92
abdeslam moukhliss <abdomokh38@gmail.com>
2024-07-30 21:22:06 +0000

    first commit

---
+++ src/main/java/org/test/gatewayservice/GatewayConfiguration.java
@@ -0,0 +1,2 @@
+package org.test.gatewayservice;public class GatewayConfiguration {
+}
---
+++ src/main/java/org/test/gatewayservice/KubernetesServiceDiscoveryService.java
@@ -0,0 +1,41 @@
+package org.test.gatewayservice;
+
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@org.springframework.stereotype.Service
+public class KubernetesServiceDiscoveryService {
+
+    private final KubernetesClient kubernetesClient;
+
+    public KubernetesServiceDiscoveryService() {
+        this.kubernetesClient = new KubernetesClientBuilder().build();
+    }
+
+    public List<String> getServiceUrls(String namespace, String labelSelector) {
+        List<Service> services = kubernetesClient.services()
+                .inNamespace(namespace)
+                .withLabelSelector(labelSelector)
+                .list()
+                .getItems();
+
+        return services.stream()
+                .map(service -> service.getSpec().getClusterIP())
+                .collect(Collectors.toList());
+    }
+    public List<String> getServiceNames(String namespace, String labelSelector) {
+        List<Service> services = kubernetesClient.services()
+                .inNamespace(namespace)
+                .withLabelSelector(labelSelector)
+                .list()
+                .getItems();
+
+        return services.stream()
+                .map(service -> service.getMetadata().getName())
+                .collect(Collectors.toList());
+    }
+}
